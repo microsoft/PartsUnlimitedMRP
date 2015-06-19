b1010707d7b4070c6e1061032ead742cdc164e63
David Tesar <david.tesar@microsoft.com>
2015-06-19 15:07:21 -0700

    Adding deploy project, new CSE template

--- deploy/AzureARM/DeployTemplate.ps1
+++
@@ -1,6 +0,0 @@
-$RGName = "dcarolinuxcse"
-New-AzureResourceGroup -Name $RGName -Location 'West US'
-
-#### Before deploying Add you subscription ID #####
-New-AzureResourceGroupDeployment -Name dcarodeploy02 -ResourceGroupName $RGName -TemplateFile '.\DeployMRP-CSE.json' -TemplateParameterFile '.\DeployMRP-CSE - parameters.json'-Verbose
-Get-AzureResourceGroup -Name $RGName
