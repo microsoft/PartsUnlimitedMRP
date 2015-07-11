81ddb4f5f55adb6bd0adc0e55ce1a29b3dbf063f
James Carpinter <james.carpinter@teamintergen.com>
2015-07-11 14:57:24 +1200

    Switch install script to use Oracle Java

--- deploy/Install-MRP-app.sh
+++ deploy/Install-MRP-app.sh
@@ -1,45 +1,28 @@
-﻿#!/bin/bash
+#!/bin/bash
+
+# Auto-accept license for Java
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 
 # Install packages
+add-apt-repository ppa:webupd8team/java
 apt-get update
-apt-get install openjdk-8-jdk -y
-apt-get install openjdk-8-jre -y
+apt-get install oracle-java8-installer -y
 apt-get install mongodb -y
-apt-get install tomcat7 -y
 apt-get install wget -y
-
-# Set Java environment variables
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
 
 # Configure the storage account and container name here
 DropStorageAccountName=""
-DropContainerName=""
+DropContainerName="mrp-drops"
 AzureResource="https://$DropStorageAccountName.blob.core.windows.net/$DropContainerName/"
 
-# Download MongoRecords.js from Azure blog storage
+# Download MongoRecords.js from Azure blob storage
 wget ${AzureResource}MongoRecords.js
 
 # Wait for 10 seconds to make sure previous step is completed
 sleep 10
 
 # Add the records to ordering database on MongoDB
-mongo ordering MongoRecords.js
-
-# Change Tomcat listening port from 8080 to 9080
-sed -i s/8080/9080/g /etc/tomcat7/server.xml
-
-# Download the client WAR file
-wget ${AzureResource}mrp.war
-
-# Wait for 10 seconds to make sure previous step is completed
-sleep 10
-
-# Copy WAR file to Tomcat directory for auto-deployment
-cp mrp.war /var/lib/tomcat7/webapps
-
-# Restart Tomcat
-/etc/init.d/tomcat7 restart
+mongo --nodb MongoRecords.js
 
 # Download the Ordering Service jar from Azure storage
 wget ${AzureResource}ordering-service-0.1.0.jar
@@ -50,4 +33,4 @@ sleep 20
 # Run Ordering Service app
 java -jar ordering-service-0.1.0.jar &
 
-echo "MRP application successfully deployed. Go to http://$HOSTNAME.cloudapp.net:9080/mrp"
+echo "MRP application successfully deployed"
