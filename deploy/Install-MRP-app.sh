819c3f0e14e4b4c4bcd7f21688f6022af223c100
James Carpinter <james.carpinter@teamintergen.com>
2015-08-03 11:21:14 +1200

    Update Java install process as per PR feedback

--- deploy/Install-MRP-app.sh
+++ deploy/Install-MRP-app.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Auto-accept license for Java
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
 # Install packages
-add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:webupd8team/java -y
 apt-get update
 apt-get install oracle-java8-installer -y
 apt-get install mongodb -y
 apt-get install tomcat7 -y
 apt-get install wget -y
 
+# Set Java environment variables
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export PATH=$PATH:/usr/lib/jvm/java-8-oracle/bin
+
+# Create symlink to Java for Tomcat
+sudo ln -s /usr/lib/jvm/java-8-oracle /usr/lib/jvm/default-java
 
 # Configure the storage account and container name here
 DropStorageAccountName=""
