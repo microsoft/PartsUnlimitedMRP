8cba315af5741d04fc41efe4ccaa87cbf07be363
James Tupper <tupper.james@gmail.com>
2015-08-25 08:15:47 -0700

    Fix typo in shell script

--- deploy/Install-Build-Dependencies.sh
+++ deploy/Install-Build-Dependencies.sh
@@ -6,10 +6,11 @@ sudo apt-get install git
 sudo apt-get install gradle
 sudo apt-get install openjdk-8-jdk openjdk-8-jre mongodb
 
+# Install Node and npm
+curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+
 # Set environment variables for Java
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
-
-# Install Node and npm
-curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
-sudo apt-get isntall node npm
