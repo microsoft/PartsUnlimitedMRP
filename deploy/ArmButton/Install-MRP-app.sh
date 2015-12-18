3a23254be4e1c32dc9fb59a44ca4aca4f006fc66
Oguz Pastirmaci <oguzp@microsoft.com>
2015-12-18 15:15:01 -0800

    Fix Java path

--- deploy/ArmButton/Install-MRP-app.sh
+++ deploy/ArmButton/Install-MRP-app.sh
@@ -21,8 +21,8 @@ apt-get install tomcat7 -y
 apt-get install wget -y
 
 # Set Java environment variables
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/bin
 
 # Copy the mongoRecords.js to the correct location
 cp ./MongoRecords.js /var/lib/partsunlimited/
