7457a3802edded6fdae557d36e823cd5ebd640af
Shad Timm <shadtimm@users.noreply.github.com>
2016-07-21 14:36:41 -0700

    Removing -y from apt-key as it is invalid

--- docs/HOL_Continuous-Deployment/env/install_vsts_agent_and_mrp_dependencies.sh
+++ docs/HOL_Continuous-Deployment/env/install_vsts_agent_and_mrp_dependencies.sh
@@ -22,7 +22,7 @@ sudo apt-get install git -y
 
 # Install dotnet core
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893 -y
+sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update -y
 
 sudo apt-get install dotnet-dev-1.0.0-preview2-003121 -y
