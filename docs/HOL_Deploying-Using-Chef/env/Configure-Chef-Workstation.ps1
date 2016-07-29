640927d48c8bbce8c40e28dd6f140edcd96aad8f
Shad Timm <shadtimm@users.noreply.github.com>
2016-07-29 14:34:35 -0700

    Adding script to configure chef workstation with base dependencies

---
+++ docs/HOL_Deploying-Using-Chef/env/Configure-Chef-Workstation.ps1
@@ -0,0 +1,18 @@
+# Set PowerShell execution policy
+Set-ExecutionPolicy RemoteSigned -Force
+
+# Install Chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+refreshenv
+
+# Install Chocolatey packages
+& choco install poshgit -y
+& choco install googlechrome -y
+& choco install firefox -y
+& choco install notepadplusplus -y
+& choco install atom -y
+& choco install putty -y
+& choco install chefdk -y
+
+refreshenv
