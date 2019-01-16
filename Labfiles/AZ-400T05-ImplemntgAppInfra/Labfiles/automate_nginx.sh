986d90d704ba554e222963f5657eae2243e233d8
eamonnk <eamonn_kelly@hotmail.com>
2019-01-16 12:25:27 +0000

    added AZ400T05 labfile

--- Labfiles/AZ-400T05-ImplemntgAppInfra/Labfiles/automate_nginx.sh
+++ Labfiles/AZ-400T05-ImplemntgAppInfra/Labfiles/automate_nginx.sh
@@ -24,4 +24,4 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx
-echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+echo "Hello World from my VM Scale set" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
